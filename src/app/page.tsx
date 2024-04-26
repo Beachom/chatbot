6b516136ae0ac886b27545f2cd1a9c42db0aa3ca
Tucker Kelly <tchristiank11@gmail.com>
2024-04-26 02:18:48 -0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="absolute inset-0 flex justify-center items-center">website content</main>
+    <main className="absolute inset-0 flex justify-center items-center">future website content</main>
   );
 }
